0152b6fcbd2d2ac5a61e338e3fdfc42180a7e5c0
Chotikarn Rungviboon <chotikarn.r@live.com>
2018-08-20 11:29:48 +0700

    Update localize-router.pipe.ts

--- src/localize-router.pipe.ts
+++ src/localize-router.pipe.ts
@@ -1,7 +1,6 @@
 import { PipeTransform, Pipe, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { LocalizeRouterService } from './localize-router.service';
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/observable/forkJoin';
+import { Subscription } from 'rxjs';
 import { equals } from './util';
 
 const VIEW_DESTROYED_STATE = 128;
